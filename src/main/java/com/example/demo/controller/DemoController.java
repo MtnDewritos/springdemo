eed8f8b1c5a88f8a64fb51861843d4ddb73be97a
unknown <seb@gieser.se>
2025-02-10 11:36:36 +0100

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @GetMapping("/encrypt")
+    public String add(@RequestParam String text) {
+        return demoService.encrypt(text);
+    }
+
+}
